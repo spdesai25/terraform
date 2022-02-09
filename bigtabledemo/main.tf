f662542f2bec4de00a4a077645d5d46b2263a27e
spdesai25 <spdesai@google.com>
2022-02-09 20:39:41 +0000

    enable dataproc api and create dataproc cluster

--- bigtabledemo/main.tf
+++ bigtabledemo/main.tf
@@ -5,7 +5,8 @@ variable "gcp_service_list" {
             "bigqueryconnection.googleapis.com",
             "bigquerystorage.googleapis.com",
             "storage-component.googleapis.com",
-            "storage-api.googleapis.com"]
+            "storage-api.googleapis.com",
+            "dataproc.googleapis.com"]
 }
 
 resource "google_project_service" "gcp_services" {
@@ -28,7 +29,35 @@ resource "google_storage_bucket_object" "datafiles" {
   bucket = "${google_storage_bucket.bucket.name}"
   source = "./datasets/${each.value}"
 }
+
+resource "google_dataproc_cluster" "mycluster" {
+  name     = "biglake-demo-cluster"
+  region = "us-central1"
+
+  cluster_config {
+    gce_cluster_config{
+        metadata = {
+            bigquery-connector-url = "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar" 
+            spark-bigquery-connector-url = "gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar"           
+        }
+    }
+    software_config {
+      optional_components = ["ZEPPELIN"]
+    }
+    initialization_action {
+      script      = "gs://goog-dataproc-initialization-actions-us-central1/connectors/connectors.sh"
+      timeout_sec = 500
+    }
+  } 
+}
+
 /*
+resource "google_bigquery_connection" "connection" {
+    provider      = google-beta
+    type = "CLOUD_RESOURCE"
+    name = "my-connection"
+}
+
 resource "google_storage_bucket_object" "datafile" {
   name = "airportdata"
   source = "./datasets/airlines_data.csv"
